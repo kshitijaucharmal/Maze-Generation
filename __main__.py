083026a0609f5ebce3e4b643ea5b837a60f7fd06
kshitijaucharmal <kshitijaucharmal21@gmail.com>
2023-06-08 16:49:27 +0530

    Removed rays

--- __main__.py
+++ __main__.py
@@ -15,47 +15,19 @@ clock = pygame.time.Clock()
 
 maze = MazeGenerator(WIDTH, HEIGHT, ROWS)
 
-rays = []
 pos = pygame.Vector2(15, 15)
 
 def main():
     run = True
     # done = False
 
-    rays.append(Ray(pygame.Vector2(20, 20), 90))
-    rays.append(Ray(pygame.Vector2(20, 20), -90))
-    rays.append(Ray(pygame.Vector2(20, 20), 45))
-    rays.append(Ray(pygame.Vector2(20, 20), -45))
-    rays.append(Ray(pygame.Vector2(20, 20), 0))
-
     maze.instant_maze()
 
     while run:
         ds.fill((11, 11, 11))
 
-        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            pos.y -= 1
-        if keys[pygame.K_s]:
-            pos.y += 1
-        if keys[pygame.K_a]:
-            pos.x -= 1
-        if keys[pygame.K_d]:
-            pos.x += 1
-
-        for ray in rays:
-            ray.set(pos)
-            # ray.look_at(mouse_pos.x, mouse_pos.y)
-            closest = ray.get_closest_wall(maze.walls)
-            if closest:
-                pygame.draw.line(ds, (255, 255, 0), ray.pos, closest)
-                pygame.draw.circle(ds, (255, 0, 0), closest, 4)
-
         # Drawing
         maze.draw(ds)
-        ray.draw(ds)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
